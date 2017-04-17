bcdffd9ef2033eb02666c08b73c51cb58edfda2f
hexiang6666 <hexwang@ucdavis.edu>
2017-04-16 23:12:12 -0700

    add more features

--- SRC/string_operator.cpp
+++ SRC/string_operator.cpp
@@ -6,7 +6,7 @@
 * Rev:               Version 1                                   | jeremic@ucdavis.edu                  *
 * Email:             hexwang@ucdavis.edu                         | Computational Geomechanics Group     *
 * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * 
-*                           Last Modified time: 2017-03-07 20:38:17                                     *            
+*                           Last Modified time: 2017-03-28 13:09:33                                     *            
 *  * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *         
 * The copyright to the computer program(s) herein is the property of Hexiang Wang and Boris Jeremic     *
 * The program(s) may be used and/or copied only with written permission of Hexiang Wang or in accordance* 
@@ -58,6 +58,45 @@ vector<string> string_operator::string_extractor(string& s1, string opening_flag
 		if(temp==closing_flag)
 		{
 			temp_string=delete_space(temp_string);    // this line means our extraction output without spaces.
+			ret.push_back(temp_string);
+			temp_string="";
+			opening_flag_it=s1.end();
+		}
+	}
+
+	return ret;
+}
+
+vector<string> string_operator::string_extractor_with_spaces(string& s1, string opening_flag, string closing_flag)
+{
+	int index=0;
+	vector<string> ret;
+	string::iterator it=s1.begin();
+	string::iterator opening_flag_it=s1.end();
+	string temp_string;
+	for (; it!=s1.end(); ++it)
+	{
+	// convert a char to string
+		// stringstream ss;
+		// string temp;
+		// ss<<(*it);
+		// ss>>temp;	
+		string temp=get_string_component(it);
+	//####################### 
+		if(temp==opening_flag)
+		{
+			index=index+1;
+			opening_flag_it=it;
+			
+		}
+		if((it>opening_flag_it)&&(temp!=closing_flag))
+		{
+			temp_string=temp_string+temp;
+
+		}
+		if(temp==closing_flag)
+		{
+			// temp_string=delete_space(temp_string);    // comment this line means our extraction output with spaces.
 			ret.push_back(temp_string);
 			temp_string="";
 			opening_flag_it=s1.end();
@@ -196,6 +235,18 @@ void string_operator::set_string(string& str)
 {
 	this->s=str;
 }
+
+
+string string_operator::match_reg(std::regex e, std::regex_iterator<std::string::iterator> it, int component_index)
+{
+	string temp= it->str();
+	std::smatch sm;
+	std::regex_match(temp,sm,e);
+	string ret=sm[component_index];
+	return ret;
+}
+
+
 
 void string_operator::balanced_replace(string& insert_str, string& s1, string& opening_flag, string& closing_flag)
 {
--- SRC/string_operator.h
+++ SRC/string_operator.h
@@ -6,7 +6,7 @@
 * Rev:               Version 1                                   | jeremic@ucdavis.edu                  *
 * Email:             hexwang@ucdavis.edu                         | Computational Geomechanics Group     *
 * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * 
-*                           Last Modified time: 2017-03-04 19:39:52                                     *            
+*                           Last Modified time: 2017-03-28 13:10:47                                     *            
 *  * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *         
 * The copyright to the computer program(s) herein is the property of Hexiang Wang and Boris Jeremic     *
 * The program(s) may be used and/or copied only with written permission of Hexiang Wang or in accordance* 
@@ -22,6 +22,7 @@
 #include <fstream>
 #include <streambuf>
 
+
 using namespace std;
 class string_operator
 {
@@ -30,6 +31,7 @@ class string_operator
 		~string_operator();
 		string_operator(const string& str);
 		std::vector<string> string_extractor(string& s1, string opening_flag, string closing_flag);
+		std::vector<string> string_extractor_with_spaces(string& s1, string opening_flag, string closing_flag);
 		string delete_space(string& s2);
 		string get_string_component(string::iterator it);
 		string get_string();
@@ -40,6 +42,8 @@ class string_operator
 		string balanced_extractor(string& s1, string& opening_flag, string& closing_flag);    //s1 is the beginning string before opening_flag
 		void balanced_replace(string& insert_str, string& s1, string& opening_flag, string& closing_flag);   //s1 is the beginning string before opening_flag
 		std::vector<string> string_separator(string& s1, string separator);
+		string match_reg(std::regex e, std::regex_iterator<std::string::iterator> it, int component_index);   //index begins from 0; 0 corresponds to the whole matched string
+
 	private:
 		string s;
 
